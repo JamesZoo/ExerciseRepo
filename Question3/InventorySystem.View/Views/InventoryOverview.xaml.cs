42f7ab394341c7627565039a9f7c1763ab89d422
Jiping ZHU <jiping.zhu@aveva.com>
2021-02-24 21:06:48 +1100

    InventoryOverview Skeleton Layout

---
+++ Question3/InventorySystem.View/Views/InventoryOverview.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace InventorySystem.View.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for InventoryOverview.xaml
+    /// </summary>
+    public partial class InventoryOverview : UserControl
+    {
+        public InventoryOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
