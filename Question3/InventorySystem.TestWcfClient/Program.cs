efdacf62044a661a6bc28450efe8abde13a05b89
Jiping ZHU <jiping.zhu@aveva.com>
2021-02-24 14:24:36 +1100

    test wcf client

--- Question3/InventorySystem.TestWcfClient/Program.cs
+++ Question3/InventorySystem.TestWcfClient/Program.cs
@@ -2,11 +2,28 @@
 
 namespace InventorySystem.TestWcfClient
 {
+    using System.ServiceModel;
+    using System.Threading.Tasks;
+    using Proxy;
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new InventoryServiceClient(new WSHttpBinding(), new EndpointAddress("http://localhost:12345/WCF/InventoryService.svc"));
+
+            try
+            {
+                var result = await client.GetInventoryInfoAsync().ConfigureAwait(false);
+                Console.WriteLine(result.LastUpdateTime);
+            }
+            catch (CommunicationException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Press ENTER to end...");
+            Console.ReadLine();
         }
     }
 }
