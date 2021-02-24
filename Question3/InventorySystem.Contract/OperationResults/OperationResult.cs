47b8465a406c2396abef70fca999fdebaff9bd62
Jiping ZHU <jiping.zhu@aveva.com>
2021-02-24 23:03:23 +1100

    Introduce ErrorCode in WCF operation result, and remove the need to try catch communication exception in AutoRecoveryClient class. Instead Disconnected error code is returned.

--- Question3/InventorySystem.Contract/OperationResults/OperationResult.cs
+++ Question3/InventorySystem.Contract/OperationResults/OperationResult.cs
@@ -3,10 +3,16 @@
     using System;
     using System.Runtime.Serialization;
 
+
+
     [DataContract]
     public class OperationResult
     {
-        ////TODO: Introduce ErrorCode for error handling.
+        /// <summary>
+        /// Gets or sets the error code of this operation result. Default is ErrorCode.Success.
+        /// </summary>
+        [DataMember]
+        public ErrorCode ErrorCode { get; set; }
 
         /// <summary>
         /// Gets or sets the timestamp indicating when the last update to the inventory system was made.
