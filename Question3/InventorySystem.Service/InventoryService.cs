55afb34fc77357e4e6baae76f102091546bc07c7
Jiping ZHU <jiping.zhu@aveva.com>
2021-02-24 11:00:59 +1100

    Simplify client / service application.

--- Question3/InventorySystem.Service/InventoryService.cs
+++ Question3/InventorySystem.Service/InventoryService.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ServiceModel;
     using System.Threading.Tasks;
     using InventorySystem.Contract;
 
---
+++ Question3/InventorySystem.TestWcfClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventorySystem.TestWcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
